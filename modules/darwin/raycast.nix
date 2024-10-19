ec19a99bbaaab393e1a33ad274e9daf6174c6849
Scott <17575677+stl314159@users.noreply.github.com>
2024-10-19 17:04:03 -0400

    Also remove show finder search

--- modules/darwin/raycast.nix
+++ modules/darwin/raycast.nix
@@ -18,5 +18,15 @@
       -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 49" \
       -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 1048576" \
       -c "Add :AppleSymbolicHotKeys:64:type string standard"
+
+    # Unmap "Show Finder search"
+    /usr/libexec/PlistBuddy ${user.home}/Library/Preferences/com.apple.symbolichotkeys.plist \
+      -c "Delete :AppleSymbolicHotKeys:65" \
+      -c "Add :AppleSymbolicHotKeys:65:enabled bool false" \
+      -c "Add :AppleSymbolicHotKeys:65:value:parameters array" \
+      -c "Add :AppleSymbolicHotKeys:65:value:parameters: integer 65535" \
+      -c "Add :AppleSymbolicHotKeys:65:value:parameters: integer 49" \
+      -c "Add :AppleSymbolicHotKeys:65:value:parameters: integer 1572864" \
+      -c "Add :AppleSymbolicHotKeys:65:type string standard"
   '';
 }
