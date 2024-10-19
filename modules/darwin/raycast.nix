368cb8714702b4b26180f9da9e0b3fb569da64aa
Scott <17575677+stl314159@users.noreply.github.com>
2024-10-19 17:03:12 -0400

    Try this to remap cmd+space

--- modules/darwin/raycast.nix
+++ modules/darwin/raycast.nix
@@ -10,7 +10,13 @@
 
   system.activationScripts.extraActivation.text = ''
     # Let Raycast have cmd+space instead of Spotlight
-    /usr/libexec/PlistBuddy ${user.home}/Library/Preferences/com.apple.symbolichotkeys.plist -c \
-    "Set AppleSymbolicHotKeys:64:enabled false"
+    /usr/libexec/PlistBuddy ${user.home}/Library/Preferences/com.apple.symbolichotkeys.plist \
+      -c "Delete :AppleSymbolicHotKeys:64" \
+      -c "Add :AppleSymbolicHotKeys:64:enabled bool false" \
+      -c "Add :AppleSymbolicHotKeys:64:value:parameters array" \
+      -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 65535" \
+      -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 49" \
+      -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 1048576" \
+      -c "Add :AppleSymbolicHotKeys:64:type string standard"
   '';
 }
